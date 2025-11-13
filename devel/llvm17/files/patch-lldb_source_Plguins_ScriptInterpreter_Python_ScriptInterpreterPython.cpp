1daac617a55f1e55046f2c9d73b5f87cd11460a4
Kelvin Lee <kiyolee@gmail.com>
2025-11-13 23:44:49 +1100

    devel/llvm17: Fix to support Python 3.14

---
+++ devel/llvm17/files/patch-lldb_source_Plguins_ScriptInterpreter_Python_ScriptInterpreterPython.cpp
@@ -0,0 +1,29 @@
+--- lldb/source/Plugins/ScriptInterpreter/Python/ScriptInterpreterPython.cpp.orig	2023-11-28 19:52:28.000000000 +1100
++++ lldb/source/Plugins/ScriptInterpreter/Python/ScriptInterpreterPython.cpp	2025-11-13 23:37:54.490161000 +1100
+@@ -177,18 +177,26 @@
+       return;
+ #endif
+ 
++#if (PY_MAJOR_VERSION == 3 && PY_MINOR_VERSION < 9) || (PY_MAJOR_VERSION < 3)
+     if (PyEval_ThreadsInitialized()) {
++#else
++    if (true) {
++#endif
+       Log *log = GetLog(LLDBLog::Script);
+ 
+       m_was_already_initialized = true;
+       m_gil_state = PyGILState_Ensure();
+       LLDB_LOGV(log, "Ensured PyGILState. Previous state = {0}locked\n",
+                 m_gil_state == PyGILState_UNLOCKED ? "un" : "");
++#if (PY_MAJOR_VERSION == 3 && PY_MINOR_VERSION < 9) || (PY_MAJOR_VERSION < 3)
+       return;
+     }
+ 
+     // InitThreads acquires the GIL if it hasn't been called before.
+     PyEval_InitThreads();
++#else
++    }
++#endif
+   }
+ 
+   PyGILState_STATE m_gil_state = PyGILState_UNLOCKED;
